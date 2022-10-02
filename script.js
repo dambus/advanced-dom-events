3b9b4b7aa29d918b88d16889519dc543b95c3593
dambus <dambusns@gmail.com>
2022-10-02 11:28:48 +0200

    sunday

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const header = document.querySelector('.header');
-
+const operations = document.querySelector('.operations');
+const nav = document.querySelector('.nav');
 //////////////////////////////////////////////////////
 ///////////////       MODAL WINDOW      //////////////
 //////////////////////////////////////////////////////
@@ -52,28 +53,69 @@ btnScrollTo.addEventListener('click', function (e) {
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   );
-  //SCROLLING
-  // window.scrollTo(s1coords.left, s1coords.top + window.scrollY);
-  // uvek uzeti u obzir trenutnu scroll poziciju, odnosno dodati je na .top poziciju da bi uvek bio isti rezultat skrolovanja sa bilo koje trenutne pozicije
-
-  //SCROLLING SMOOTH
-  // window.scrollTo({
-  //   left: s1coords.left + window.scrollX,
-  //   top: s1coords.top + window.scrollY,
-  //   behavior: 'smooth',
-  // });
-
-  section1.scrollIntoView({ behavior: 'smooth' }); //MODERNI NACIN, ako ne rade na starom browseru, koristiti prethodni nacin
 });
+
+//////////////////////////////////////////////////////
+////////////       TABBED COMPONENT      /////////////
+//////////////////////////////////////////////////////
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+//event delegation
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return; //guard clause ako nije kliknuto na dugme odmah return i prekida se operacij - ne desava se nista. Ako je kliknuto na dugme, nastavlja se izvrsavanje koda
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  // Activating content area
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+//////////////////////////////////////////////////////
+////////////     MENU FADE ANIMATION     /////////////
+//////////////////////////////////////////////////////
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 0.5;
+    });
+    logo.style.opacity = 0.5;
+  }
+});
+nav.addEventListener('mouseout', function (e) {});
+// REFERENCE CLASSES
+//SCROLLING
+// window.scrollTo(s1coords.left, s1coords.top + window.scrollY);
+// uvek uzeti u obzir trenutnu scroll poziciju, odnosno dodati je na .top poziciju da bi uvek bio isti rezultat skrolovanja sa bilo koje trenutne pozicije
+
+//SCROLLING SMOOTH
+// window.scrollTo({
+//   left: s1coords.left + window.scrollX,
+//   top: s1coords.top + window.scrollY,
+//   behavior: 'smooth',
+// });
+
+section1.scrollIntoView({ behavior: 'smooth' }); //MODERNI NACIN, ako ne rade na starom browseru, koristiti prethodni nacin
+
 //////////////////////////////////////////////////////
 //////////       PAGE NAVIGATION       ///////////////
 //////////////////////////////////////////////////////
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
+  }
 });
 //////////////////////////////////////////////////////
 // KREIRANJE I BRISANJE HTML ELEMENATA
@@ -177,4 +219,33 @@ console.log(randomColor(0, 255));
 // document.querySelector('.nav').addEventListener('click', function (e) {
 //   this.style.backgroundColor = randomColor(0, 255);
 //   console.log('NAV', e.target, e.currentTarget);
+// });
+
+// // DOM TRAVERSING
+// //const h1 = document.querySelector('h1'); //alredy selected
+
+// // Going downwards
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+
+// //going upwards
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// // closest find closest parent to the element passed into function .header in this case, so closest heaeder to selected h1 element
+
+// // going sideways (siblings)
+// // only direct siblings can be selected
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// // if all siblings are neded, there is a trick
+// console.log(h1.parentElement.children); //includes h1
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) {
+//     el.style.transform = 'scale(0.5)';
+//   }
 // });
